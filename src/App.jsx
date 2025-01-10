b8dcfcd708523b3d56c6644f4b45a10cf6f8494c
Acastillooo <110639309+Acastillooo@users.noreply.github.com>
2025-01-10 09:53:41 -0300

    Create App.jsx
    .

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DynatraceDashboard from './components/DynatraceDashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <DynatraceDashboard />
+    </div>
+  );
+}
+
+export default App;
